02e75642a06eb237eff63906d846e5b4e67e0272
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-03-19 12:21:59 +0100

    Add secret and vault profile to compliance

--- src/main/java/com/czertainly/api/model/core/auth/Resource.java
+++ src/main/java/com/czertainly/api/model/core/auth/Resource.java
@@ -102,8 +102,8 @@ public enum Resource implements IPlatformEnum {
     ;
 
     private static final Resource[] VALUES;
-    private static final EnumSet<Resource> complianceSubjects = EnumSet.of(Resource.CERTIFICATE, Resource.CERTIFICATE_REQUEST, Resource.CRYPTOGRAPHIC_KEY);
-    private static final EnumSet<Resource> complianceProfilesAssignable = EnumSet.of(Resource.RA_PROFILE, Resource.TOKEN_PROFILE);
+    private static final EnumSet<Resource> complianceSubjects = EnumSet.of(Resource.CERTIFICATE, Resource.CERTIFICATE_REQUEST, Resource.CRYPTOGRAPHIC_KEY, Resource.SECRET);
+    private static final EnumSet<Resource> complianceProfilesAssignable = EnumSet.of(Resource.RA_PROFILE, Resource.TOKEN_PROFILE, Resource.VAULT_PROFILE);
 
     static {
         VALUES = values();
